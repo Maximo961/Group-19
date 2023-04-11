a1b75b380ed0d589421fcb643b2b0e86c33005b9
Massimo Q <68969037+MQ40@users.noreply.github.com>
2023-04-11 13:52:19 -0400

    Update BookstoreApplication.java

--- bookstore/src/main/java/com/group19/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/group19/bookstore/BookstoreApplication.java
@@ -2,44 +2,12 @@ package com.group19.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.sql.*;
-
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-		Connection connection = null;
-		try {
-			// below two lines are used for connectivity.
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/sql_bookinventory",
-					"root", "password");
-
-			// mydb is database
-			// mydbuser is name of database
-			// mydbuser is password of database
-
-			Statement statement;
-			statement = connection.createStatement();
-			ResultSet resultSet;
-			// Place query here
-			resultSet = statement.executeQuery(
-					"SELECT * FROM sql_bookinventory.users;");
-			String email;
-			while (resultSet.next()) {
-				email = resultSet.getString("email").trim();
-				System.out.println("Emails : " + email);
-			}
-			resultSet.close();
-			statement.close();
-			connection.close();
-		}
-		catch (Exception exception) {
-			System.out.println(exception);
-		}
 	}
 
 }
